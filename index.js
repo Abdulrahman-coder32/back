4f6abfa3338d0fc13a35591c7fedb6e7eff239fe
Abdulrahman-coder32 <g5rys27ff5@privaterelay.appleid.com>
2026-06-04 13:30:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -142,7 +142,7 @@ io.on('connection', (socket) => {
 // ────────────────────────────────────────
 app.use(express.static(path.join(__dirname, 'fadahrak-frontend/dist/fadahrak-frontend')));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'fadahrak-frontend/dist/fadahrak-frontend/index.html'));
 });
 
